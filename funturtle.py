a049ed99e4ba1750e36c2b898a91bdeee23aaf29
gaya19-meet <gaya19@meet.mit.edu>
2017-07-31 17:30:32 +0200

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -5,13 +5,26 @@ square = turtle.clone () #creats new turtle and saves it in square
 square.shape('square') #changes shape of 2nd turtle to square
 square.goto(100,100) #moves square to (x=100,y=100)
 
+turtle.pendown()
+
 # draw a square
 square.goto(0,0)
 square.goto(50,0)
 square.goto(50,50)
 square.goto(0,50)
 square.goto(0,0)
+square.penup()
+
+
+turtle.shape('triangle')
 triangle = turtle.clone()
+triangle.penup()
+triangle.goto(-100,-100)
+triangle.pendown()
+triangle.goto(-100,0)
+triangle.goto(0,-100)
+triangle.goto(-100,-100)
+triangle.penup()
 
 
 turtle.mainloop()
